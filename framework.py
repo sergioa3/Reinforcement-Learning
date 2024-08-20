61b72ef21481741738e8d91f82f6cba050d8c518
sergioa3 <111718931+sergioa3@users.noreply.github.com>
2024-08-20 14:39:51 -0500

    Initial framework.py along with 2d gridworld 'plane' example.
    Framework.py contains all the general framework along with implemented algorithms. Currently only sarsa is implemented. Coming days will include q-learning and expected sarsa. Currently the Q-greedy policies used within the algorithms will be epsilon-greedy policies with arbitrary breakups between argmax actions.

---
+++ framework.py
@@ -0,0 +1,394 @@
+import numpy as np
+
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+class distribution:
+    def __init__(self, distribution_dict:dict) -> None:
+        d_sum = np.array(list(distribution_dict.values())).sum()
+        if d_sum != 1:
+            raise Exception('Distribution must sum 1, got {}'.format(d_sum))
+        self.dictio = distribution_dict
+
+    def get_state(self):
+        values = np.array(list(self.dictio.keys()))
+        probabilities = list(self.dictio.values())
+
+        # Generate a sample
+        sample = np.random.choice(values, p=probabilities)
+        return sample
+    
+    def get_possible_states(self) -> dict.keys:
+        return self.dictio.keys()
+    
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+
+class kernel:
+    def __init__(self, state_action_distributions:dict) -> None:
+        self.state_action_distributions = state_action_distributions
+        #state_action_distributions must be a dictionary with tuple keys (state, action) and values of class distribution
+    
+    def get_state(self, state, action):
+        try:
+            sample = self.state_action_distributions[(state, action)].get_state()
+        except Exception as e:
+            raise e
+            
+        return sample
+    
+    def get_distribution(self, state, action)-> distribution:
+        try:
+            dist = self.state_action_distributions[(state, action)]
+        except Exception as e:
+            raise e
+        return dist
+    
+    def get_state_actions(self) -> dict.keys:
+        return self.state_action_distributions.keys()
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+
+class rewards:
+    def __init__(self, reward_funtion:dict) -> None:
+        self.reward_function = reward_funtion
+        #reward funtion must be a dictionary with tuple keys (state, action) and values of class float
+
+    def get_reward(self, state, action):
+        try:
+            reward = self.reward_function[(state, action)]
+        except Exception as e:
+            raise e
+            
+        return reward
+    
+    def get_state_actions(self) -> dict.keys:
+        return self.reward_function.keys()
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+
+#episodic environments will be represented by environments which have absorving states.
+#what is an absorving state?
+# A state s whose distribution for any action a looks like this: {s:1}.
+
+#that means we are assuming that absorving states are terminal states
+
+
+#the environment will check for absorving states from the begining and tag them
+
+class environment:
+    def __init__(self, kernel:kernel , rewards:rewards, d0:distribution) -> None:
+        
+        state_actions_k = kernel.get_state_actions()
+        state_actions_r = rewards.get_state_actions()
+
+        
+        #the set of states must be given by all the possible entries of state in the kernel and the rewards
+        #and the consistency of these must be checked:
+
+        # Extract states and actions from state_actions_k and state_actions_r
+        states_k = {sa[0] for sa in state_actions_k}
+        actions_k = {sa[1] for sa in state_actions_k}
+
+        states_r = {sa[0] for sa in state_actions_r}
+        actions_r = {sa[1] for sa in state_actions_r}
+
+
+        # Compare the sets of states and actions
+        same_states = states_r == states_k
+        same_actions = actions_r == actions_k
+
+        if not (same_states):
+            raise Exception('States from kernel are not consistent with states of reward function.')
+        
+        if not (same_actions):
+            raise Exception('Actions from kernel are not consistent with actions of reward function.')
+        
+        if not (set(state_actions_r)==set(state_actions_k)):
+            raise Exception('State-action pairs from kernel are not consistent with state-action pairs of reward function.')
+        
+
+        # New dictionary to store available actions for each state
+        state_actions = {}
+
+        # Populate the new dictionary
+        for (state, action) in state_actions_r:
+            if state not in state_actions:
+                state_actions[state] = set()  # Initialize with an empty set
+            state_actions[state].add(action)
+
+        self.state_actions = state_actions
+        del states_r
+        del actions_r
+        del states_k
+        del actions_k
+        del state_actions_r
+        del state_actions_k
+        
+        
+        # Check if d0 states is a subset of states
+        if not set(d0.get_possible_states()).issubset(set(self.get_all_states())):
+            raise Exception('d0 must be a subset of the possible states.')
+        
+        self.d0 = d0
+        self.kernel = kernel
+        self.rewards = rewards
+
+
+        #tag absorving/terminal states:
+        terminal_states = set()
+        for s in self.get_all_states():
+            all_possible_next_states = set()
+            actions = self.get_actions(s)
+            for a in actions:
+                all_possible_next_states.update(set(self.kernel.get_distribution(s,a).get_possible_states()))
+            
+            if all_possible_next_states == set([s]):
+                #state is absorving/terminal
+                terminal_states.add(s)
+
+        self.terminal_states = terminal_states
+
+        
+    def get_all_states(self)-> dict.keys:
+        return self.state_actions.keys()
+    
+    def get_actions(self, state)-> set:
+        #Actions for state
+        return self.state_actions[state]
+    
+    def get_all_actions(self)-> set:
+        # Union of all action sets across all states
+        all_actions = set.union(*self.state_actions.values())
+        return all_actions
+
+    def get_initial_state(self):
+        state = self.d0.get_state()
+
+        return state
+
+    def take_action(self, state, action)-> tuple:
+        #RETURN WILL BE A TRIPLE
+        #(state, reward, boolean)
+        #boolean indicates if the state inputed was terminal and therefore environment must me restarted by agent
+        terminal = False
+
+        if state in self.terminal_states:
+            terminal = True
+        try:
+            s_next = self.kernel.get_state(state, action)
+            r = self.rewards.get_reward(state, action)
+        except KeyError:
+            raise KeyError('Action "{}" not recognized for state "{}" by environment.'.format(action,state))
+        
+        return (s_next, r, terminal)
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+
+class Q_Estimate:
+    def __init__(self, environment, action0=None) -> None:
+        #structure must be a mapping from (state, action) to values
+        state_actions = environment.state_actions
+        estimate = {}
+        for state in set(state_actions.keys()):
+            estimate[state] = {}
+            for action in state_actions[state]:
+                sa = 0
+                if action0:
+                    if action in action0:
+                        sa = 0.01
+                estimate[state][action] = sa
+        
+        self.estimate = estimate
+
+    def update(self, state, action, sa)-> None:
+        if not state in set(self.estimate.keys()):
+            raise KeyError('State "{}" not recognized by Q.'.format(state))
+        
+        if not action in set(self.estimate[state].keys()):
+            raise KeyError('Action "{}" not recognized for state "{}" by Q'.format(action, state))
+        
+        self.estimate[state][action] = sa
+
+        return None
+    
+    def get_states(self):
+        return set(self.estimate.keys())
+    
+    def argmax_a(self, state)-> tuple:
+        #return tuple of (action, value) of maximum Q_estimate value for state
+        if not state in set(self.estimate.keys()):
+            raise Exception('State "{}" not recognized by Q.'.format(state))
+
+        actions = set(self.estimate[state].keys())
+
+        argmax = np.random.choice(np.array(list(actions)))
+        max = self.get(state, argmax)
+        for a in actions:
+            sa = self.get(state, a)
+            if sa > max:
+                argmax = a
+                max = sa
+        return (argmax, max)
+
+
+    def get(self, state, action)-> float:
+        try:
+            action_values = self.estimate[state]
+        except KeyError:
+            raise KeyError('State "{}" not recognized by Q.'.format(state))
+        
+        try:
+            sa = action_values[action]
+        except KeyError:
+            raise KeyError('Action "{}" not recognized for state "{}" by Q'.format(action, state))
+        
+        return sa
+
+        
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+
+class policy:
+    def __init__(self, environment) -> None:
+        #structure must be a mapping from states to actions
+        state_actions = environment.state_actions
+        pi = {}
+        for state in set(state_actions.keys()):
+            actions = state_actions[state]
+            #choose random action
+            any_action = np.random.choice(np.array(list(actions)))
+            pi[state] = any_action
+        
+        self.pi = pi
+
+    def __init__(self, environment:environment, Q:Q_Estimate, epsilon:float = 0) -> None:
+        #structure must be a mapping from states to actions
+        state_actions = environment.state_actions
+        pi = {}
+
+        if not (0<=epsilon and epsilon<=1):
+            raise Exception('Epsilon must be between zero and 1, received {}'.format(epsilon))
+        
+        for state in set(state_actions.keys()):
+            actions = state_actions[state]
+            #with probability epsilon choose a random action
+            if np.random.binomial(n=1, p=1-epsilon)==1:
+                #choose Q greedy action
+                action = Q.argmax_a(state)[0]
+            else:
+                #choose random action
+                action = np.random.choice(np.array(list(actions)))
+            pi[state] = action
+        
+        self.pi = pi
+    
+    def update(self, state, action):
+        #this will check if the state is recognized by policy
+        if not state in set(self.pi.keys()):
+            raise Exception('State "{}" not recognized by policy.'.format(state))
+        
+        #however the policy will not check wether the action is recognized by environment
+        
+        self.pi[state] = action
+
+    def get_action(self, state):
+        #this will check if the state is recognized by policy
+        try:
+            action = self.pi[state] 
+        except KeyError:
+            raise KeyError('State "{}" not recognized by policy.'.format(state))
+        return action
+        
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+  
+class agent:
+    def __init__(self, environment: environment, discount:float, history_callback: callable = None, action0 = None) -> None:
+        
+        if not (0<=discount and discount<=1):
+            raise Exception('Discount factor must be between zero and 1, received {}'.format(discount))
+        
+        self.environment = environment
+        self.cummulative_reward = 0
+        self.discount = discount
+
+        self.state = self.environment.get_initial_state()
+        if action0:
+            self.Q = Q_Estimate(environment, action0)
+        else:
+            self.Q = Q_Estimate(environment)
+        self.pi = policy(environment, self.Q, epsilon=0)
+
+#        print(self.Q.estimate)
+        print(self.pi.pi)
+
+        self.running = False
+        self.history = [self.state]
+
+        self.history_callback = history_callback
+        
+
+    def restart(self):
+        self.cummulative_reward = 0
+        self.state = self.environment.get_initial_state()
+        self.history = [self.state]
+        
+    def update(self, state, r):
+        self.state = state
+        self.history.append(state)
+        self.cummulative_reward = self.cummulative_reward + r
+        if self.history_callback:
+            self.history_callback(self.history, self.cummulative_reward)
+
+    
+
+    def sarsa(self, alpha, epsilon):
+        self.running = True
+        while True:
+            print(self.cummulative_reward)
+            self.restart()
+            terminated = False
+            s = self.state
+            
+            if np.random.binomial(n=1, p=1-epsilon)==1:
+                #choose Q greedy action
+                a = self.pi.get_action(s)
+            else:
+                #choose random action
+                actions = self.environment.get_actions(s)
+                a = np.random.choice(np.array(list(actions)))
+
+            while not terminated:
+                
+                s_next, r, terminated = self.environment.take_action(s, a)
+                #print(s, a, s_next)
+
+
+
+                if np.random.binomial(n=1, p=1-epsilon)==1:
+                    #choose Q greedy action
+                    a_next = self.pi.get_action(s_next)
+                else:
+                    #choose random action
+                    actions = self.environment.get_actions(s_next)
+                    a_next = np.random.choice(np.array(list(actions)))
+
+                q = self.Q.get(s, a)
+                q_next = self.Q.get(s_next, a_next)
+                new_q = q + alpha * ( r + self.discount*q_next - q )
+
+                self.Q.update(s,a, new_q)
+                if new_q > q:
+                    self.pi.update(s, self.Q.argmax_a(s)[0])
+
+                s = s_next
+                a = a_next
+                self.update(s, r)
+                
+            
+
---
+++ plane.py
@@ -0,0 +1,220 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+from framework import distribution, kernel, rewards, environment, agent
+
+
+
+
+n = 15
+m = 10
+
+
+reward_dict = {}
+
+distribution_dict = {}
+
+import math
+
+def objective_region(i,j):
+    objective = n*(17/20) <= i <= n
+    objective = objective and m*(12/15) <= j <= m
+    return objective
+
+def danger_region(i,j):
+    danger1 = n*(13/20) <= i <= n*(15/20)
+    danger1 = danger1 and m*(8/15) <= j <= m
+
+    danger2 = n*(6/20) <= i <= n*(8/20)
+    danger2 = danger2 and m*(2/15) <= j <= m*(6/15)
+
+    danger = danger1 or danger2
+
+    #return danger
+    return diamond(i,j, -n/5, -m/5) or diamond(i,j, n/10, m/5)
+
+
+def circular(i, j):
+    centers = [(n//4, m//4), (3*n//4, 3*m//4), (n//2, m//2)]
+    radius = min(n, m) // 8
+    for cx, cy in centers:
+        if (i - cx)**2 + (j - cy)**2 < radius**2:
+            return True
+    return False
+
+def checkerboard(i, j):
+    block_size = min(n, m) // 10
+    return ((i // block_size) % 2) == ((j // block_size) % 2)
+
+def wavy(i, j):
+    amplitude = m // 10
+    wavelength = n // 5
+    wave = amplitude * math.sin(2 * math.pi * i / wavelength) + m // 2
+    return abs(j - wave) < amplitude
+
+def concentric(i, j):
+    cx, cy = n//2, m//2
+    distance = math.sqrt((i - cx)**2 + (j - cy)**2)
+    ring_width = n // 10
+    return int(distance // ring_width) % 2 == 0
+
+def ellipse(i, j):
+    ellipses = [
+        (n // 4, m // 4, n // 8, m // 12),
+        (3 * n // 4, 3 * m // 4, n // 6, m // 10),
+        (n // 2, m // 2, n // 5, m // 8)
+    ]
+    for cx, cy, rx, ry in ellipses:
+        if ((i - cx) / rx)**2 + ((j - cy) / ry)**2 < 1:
+            return True
+    return False
+
+def diamond(i, j, x, y):
+    cx, cy = n // 2 + x, m // 2 + y  # Center of the grid
+    return abs(i - cx) + abs(j - cy) < n // 4
+
+
+
+
+
+
+
+for i in range(n):
+    for j in range(m):
+        #print(i*m+j)
+        #print(i,j)
+        s = i*m + j
+
+        up = (i+1)*m + j
+        down = (i-1)*m + j
+        left = i*m + (j - 1)
+        right = i*m + (j + 1)
+        if objective_region(i,j):
+            reward_dict[(s,'u')] = 0
+            u_dist = {s:1}
+            distribution_dict[(s,'u')] = distribution(u_dist)
+        else:
+
+            if i+1 < n:
+                if objective_region(i+1,j):
+                    reward_dict[(s,'u')] = 10000000
+                    u_dist = {up:1}
+                    distribution_dict[(s,'u')] = distribution(u_dist)
+                elif danger_region(i+1,j):
+                    reward_dict[(s,'u')] = -10
+                    u_dist = {up:1}
+                    distribution_dict[(s,'u')] = distribution(u_dist)
+                else:
+                    reward_dict[(s,'u')] = -5
+                    u_dist = {up:1}
+                    distribution_dict[(s,'u')] = distribution(u_dist)
+            if i-1 >= 0:
+                if objective_region(i-1,j):
+                    reward_dict[(s,'d')] = 10000000
+                    d_dist = {down:1}
+                    distribution_dict[(s,'d')] = distribution(d_dist)
+                elif danger_region(i-1,j):
+                    reward_dict[(s,'d')] = -10
+                    d_dist = {down:1}
+                    distribution_dict[(s,'d')] = distribution(d_dist)
+                else:
+                    reward_dict[(s,'d')] = -5
+                    d_dist = {down:1}
+                    distribution_dict[(s,'d')] = distribution(d_dist)
+
+            if j+1 < m:
+                if objective_region(i,j+1):
+                    reward_dict[(s,'r')] = 10000000
+                    r_dist = {right:1}
+                    distribution_dict[(s,'r')] = distribution(r_dist)
+                elif danger_region(i,j+1):
+                    reward_dict[(s,'r')] = -10
+                    r_dist = {right:1}
+                    distribution_dict[(s,'r')] = distribution(r_dist)
+                else:
+                    reward_dict[(s,'r')] = -5
+                    r_dist = {right:1}
+                    distribution_dict[(s,'r')] = distribution(r_dist)
+            
+            if j-1 >= 0:
+                if objective_region(i,j-1):
+                    reward_dict[(s,'l')] = 10000000
+                    l_dist = {left:1}
+                    distribution_dict[(s,'l')] = distribution(l_dist)
+                elif danger_region(i,j-1):
+                    reward_dict[(s,'l')] = -10
+                    l_dist = {left:1}
+                    distribution_dict[(s,'l')] = distribution(l_dist)
+                else:
+                    reward_dict[(s,'l')] = -5
+                    l_dist = {left:1}
+                    distribution_dict[(s,'l')] = distribution(l_dist)
+
+
+def plot(trajectory, score):
+    # Create the grid
+    grid = np.zeros((n, m, 3))  # RGB grid initialized to black
+
+    # Fill in the grid based on the regions
+    for i in range(n):
+        for j in range(m):
+            if danger_region(i, j):
+                grid[n-i-1, j] = [1, 0, 0]  # Red for danger region
+            elif objective_region(i, j):
+                grid[n-i-1, j] = [0, 1, 0]  # Green for objective region
+
+    # Example list of points to connect with a blue line
+    points = [(p//m, p%m) for p in trajectory]
+    #points = [(0, 0), (1, 2), (3, 4), (6, 5), (9, 9)]  # Format: (i, j)
+
+    # Extract x and y coordinates from points
+    x_coords = [point[1] for point in points]
+    y_coords = [n - point[0] - 1 for point in points]  # Adjust for grid orientation
+
+    # Plotting the grid with correct orientation
+    plt.imshow(grid, interpolation='none', origin='upper')
+    #plt.grid(True, which='both', color='black', linestyle='-', linewidth=2)
+
+    # Adjust gridlines and ticks
+    plt.xticks(np.arange(-0.5, m, 1), labels=[])
+    plt.yticks(np.arange(-0.5, n, 1), labels=[])
+    plt.gca().set_xticks(np.arange(0.5, m, 1), minor=True)
+    plt.gca().set_yticks(np.arange(0.5, n, 1), minor=True)
+    plt.gca().grid(which='minor', color='black', linestyle='-', linewidth=1)
+
+    # comment codeblock above and use this to remove grid from border 
+    #plt.gca().set_xticks([])
+    #plt.gca().set_yticks([])
+
+
+    # Remove axis labels
+    plt.gca().set_xticklabels([])
+    plt.gca().set_yticklabels([])
+
+    # Plot the blue line connecting the points
+    plt.plot(x_coords, y_coords, color='blue', marker='')
+    # Plot the latest location
+    plt.plot(x_coords[-1], y_coords[-1], color='yellow', marker='.')
+
+    #score = 95  # Example score value
+    plt.text(1.02, 0.95, "Score: {}".format(score), transform=plt.gca().transAxes, fontsize=14,
+            verticalalignment='center', horizontalalignment='left')
+
+
+    #plt.show()
+    #plt.clf()
+    plt.show()
+    plt.pause(0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
+    plt.clf()
+
+
+
+
+K = kernel(distribution_dict)
+R = rewards(reward_dict)
+d0 = distribution({0:1})
+env = environment(K,R,d0)
+#agent1 = agent(env,discount=0.9, history_callback=plot, action0=['u','r'])
+agent1 = agent(env,discount=0.9, history_callback=plot, action0=None)
+
+agent1.sarsa(alpha=0.001, epsilon=0.01)
